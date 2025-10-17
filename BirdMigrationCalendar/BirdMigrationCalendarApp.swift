24d5e1abf9e0d75f9a3af054464603c9d9ee9fbc
esplusnin <esplusnin94@yandex.ru>
2025-10-17 06:45:12 +0300

    Initial Commit

---
+++ BirdMigrationCalendar/BirdMigrationCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdMigrationCalendarApp.swift
+//  BirdMigrationCalendar
+//
+//  Created by Евгений on 17.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct BirdMigrationCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
